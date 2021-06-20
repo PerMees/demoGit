48520cf845c61fea90f80f3c9124df9f2f607f0a
PerMees <somdp2907@gmail.com>
2021-06-20 23:49:02 +0700

    create function testCompleted

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ console.log("master");
 const test = () => {
   console.log("testBranch");
 };
+
 const dev = () => {
   console.log("devBranch");
 };
 
 const checkTask = () => {
   console.log("testTask");
+};
+
+const testCompleted = () => {
+  console.log("testComplete");
 };
